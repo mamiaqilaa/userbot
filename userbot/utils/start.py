04b10eea3ace60cd9f19698c70aa7cb2ee70e363
PunyaArsya <97335354+PunyaArsya@users.noreply.github.com>
2022-05-14 22:44:33 +0700

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -3,7 +3,7 @@ from telethon import Button
 from userbot import BOTLOG, BOTLOG_CHATID, LOGS, tgbot
 
 
-async def startupmessage():
+async def startupmessage(tgbot):
     """
     Start up message in telegram logger group
     """
