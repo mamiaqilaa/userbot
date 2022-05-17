11c7307e1a7c856a5d02f7a1eecd493ec31b77fe
PunyaArsya <97335354+PunyaArsya@users.noreply.github.com>
2022-05-17 19:00:22 +0700

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -1,15 +1,15 @@
 from telethon import Button
 
-from userbot import BOTLOG, BOTLOG_CHATID, LOGS, bot
+from userbot import BOTLOG, BOTLOG_CHATID, LOGS, tgbot
 
 
-async def startupmessage(bot):
+async def startupmessage(tgbot):
     """
     Start up message in telegram logger group
     """
     try:
         if BOTLOG:
-            await bot.send_file(
+            await tgbot.send_file(
                 BOTLOG_CHATID,
                 "https://telegra.ph/file/cbe826936d4de9ec1838a.jpg",
                 caption="✨ **ALBY-Userbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 9.0@ALBY-Userbot\n➠ **Ketik** `.ping` **Untuk Mengecek Bot**\n➠ **Ketik** `.help` **Untuk Melihat Informasi Module**\n━━━━━━━━━━━\n➠ **Powered By:** @ruangprojects ",
